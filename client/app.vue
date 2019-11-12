4d6caf0e82e6daf053c05300be57a6826d7f4013
suedar <690372513@qq.com>
2019-11-12 19:01:44 +0800

    add client

---
+++ client/app.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="draw">
+        <header>DRAW</header>
+        <div class="content">
+            
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    data() {
+        return {
+            bundler: 'Parcel'
+        }
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.container {
+    color: green;
+}
+</style>
