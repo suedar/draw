d2f26fde96a6a0d72ce840ae0251b837e052585e
suedar <690372513@qq.com>
2019-12-01 15:54:22 +0800

    almosr finish client

--- client/vue/preview/carousel.vue
+++ client/vue/preview/carousel.vue
@@ -2,24 +2,24 @@
     <div class="result">
         <header>结果</header>
         <div class="content">
-            <div class="fast-prev">
+            <div class="fast-prev" :class="{disable: curIndex - 2 < 2}" @click="moveWrapper(-2, curIndex - 2 < 2)">
                 <img src="./../../assets/image/more-left.svg"></img>
             </div>
-            <div class="slow-prev">
+            <div class="slow-prev" :class="{disable: curIndex - 1 < 2}" @click="moveWrapper(-1, curIndex - 1 < 2)">
                 <img src="./../../assets/image/left.svg"></img>
             </div>
             <div class="display">
                 <ul class="wrapper" :style="{left: left + 'px'}">
-                    <li class="item" v-for="item in content" :key="item.id">
+                    <li class="item" v-for="(item, index) in data.content" :key="item.id">
                         <img :src="item.url" :alt="item.createTime">
                         <span>{{item.createTime}}</span>
                     </li>
                 </ul>
             </div>
-            <div class="slow-next">
+            <div class="slow-next" :class="{disable: curIndex + 1 > data.maxQueryIndex}" @click="moveWrapper(1, curIndex + 1 > data.maxQueryIndex)">
                 <img src="./../../assets/image/right.svg"></img>
             </div>
-            <div class="fast-next">
+            <div class="fast-next" :class="{disable: curIndex + 2 > data.maxQueryIndex}" @click="moveWrapper(2, curIndex + 2 > data.maxQueryIndex)">
                 <img src="./../../assets/image/more-right.svg"></img>
             </div>
         </div>
@@ -36,20 +36,53 @@ export default {
                 pageSize: 9,
                 pageIndex: 1
             },
-            curIndex: 2,
+            data: {
+                content: [],
+                minQueryIndex: 2,
+                maxQueryIndex: 6,
+            },
             content: [],
-            left: -110
+            curIndex: 2,
+            left: 0
         }
     },
     methods: {
-        async queryPreview() {
-            const { params } = this;
+        async queryPreview(params:Object = this.params) {
             const res = await getPreview(params);
-            this.content = res;
+            this.data = res;
+        },
+        moveWrapper(num: Number, flag: Boolean) {
+            console.log(num, flag)
+            if (flag) {
+                return;
+            }
+            let { left, curIndex, params, data } = this;
+            left -= (110 * num);
+            curIndex += num;
+            if (curIndex <= data.minQueryIndex) {
+                params.pageIndex = params.pageIndex - 1;
+                left = 0;
+                this.queryPreview(params);
+            }
+            else if (curIndex >= data.maxQueryIndex) {
+                params.pageIndex = params.pageIndex + 1;
+                left = -(params.pageSize + 2) * 110;
+                this.queryPreview(params);
+            }
+            this.left = left;
+            this.curIndex = curIndex;
+        },
+        moveAuto() {
+            const { curIndex, data } = this;
+            const flag = curIndex + 1 > data.maxQueryIndex;
+            // 遗留setIntervalbug 先写服务器
+            console.log(333)
+            setInterval(this.moveWrapper(1, flag), 100);
         }
     },
-    created() {
+    mounted() {
         this.queryPreview();
+        this.moveAuto();
     }
 }
 </script>
@@ -97,6 +130,11 @@ export default {
                     border-color: #e8b90c;
                     cursor: pointer;
                 }
+            }
+        }
+        .disable {
+            &:hover {
+                cursor: not-allowed!important;
             }
         }
         .display {
